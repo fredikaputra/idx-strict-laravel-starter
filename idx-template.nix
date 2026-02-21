fb6487f37468165d798f61d7ee5d7a8504bb6985
Fredika Putra <53744519+fredikaputra@users.noreply.github.com>
2026-02-21 16:47:27 +0700

    Update

--- idx-template.nix
+++ idx-template.nix
@@ -9,7 +9,7 @@
     bootstrap = ''
       composer create-project laravel/laravel "$out" --remove-vcs --prefer-dist --no-scripts
 			mkdir "$out"/.idx
-      sail=${toString sail} j2 ${./devNix.j2} -o "$out/.idx/dev.nix"
+      j2 ${./devNix.j2} -o "$out/.idx/dev.nix"
       chmod -R u+w "$out"
     '';
 }
