a3e6be6830edddea8d96727d706c0a8e059d0dcc
Fredika Putra <53744519+fredikaputra@users.noreply.github.com>
2026-02-21 17:03:51 +0700

    Update

--- idx-template.nix
+++ idx-template.nix
@@ -8,8 +8,26 @@
     ];
 
     bootstrap = ''
-      composer create-project laravel/laravel "$out" --remove-vcs --prefer-dist --no-scripts
-			mkdir "$out"/.idx
+      ${
+        if kit == "livewire" then
+          "composer create-project laravel/blank-livewire-starter-kit \"$out\" --stability=dev"
+        else if kit == "blade-strict" then
+          composer create-project nunomaduro/laravel-starter-kit --prefer-dist \"$out\"
+        else if kit == "react" then
+          "composer create-project laravel/blank-react-starter-kit \"$out\" --stability=dev"
+        else if kit == "react-strict" then
+          composer create-project nunomaduro/laravel-starter-kit-inertia-react --prefer-dist \"$out\"
+        else if kit == "vue" then
+          "composer create-project laravel/blank-vue-starter-kit \"$out\" --stability=dev"
+        else if kit == "vue-strict" then
+          composer create-project nunomaduro/laravel-starter-kit-inertia-vue --prefer-dist \"$out\"
+        else if kit == "svelte" then
+          "composer create-project laravel/blank-svelte-starter-kit \"$out\" --stability=dev"
+        else
+          "composer create-project laravel/laravel \"$out\" --remove-vcs --prefer-dist --no-scripts"
+      }
+
+      mkdir -p "$out"/.idx
       j2 ${./devNix.j2} -o "$out/.idx/dev.nix"
       chmod -R u+w "$out"
     '';
