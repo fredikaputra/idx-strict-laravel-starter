7ad0df3083ab15c8ceb75fee13f061b2e4b91362
Fredika Putra <53744519+fredikaputra@users.noreply.github.com>
2026-03-06 14:21:41 +0700

    Update

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ composer setup
 APP_NAME_VALUE=$(grep '^APP_NAME=' .env | cut -d '=' -f2- | tr -d '"' | tr -d "'")
 
 sed -i "s|\${APP_NAME}|$APP_NAME_VALUE|g" .env
-sed -i "s|^APP_URL=.*|APP_URL=https://9000-$WEB_HOST|" .env
-sed -i "s|^ASSET_URL=.*|ASSET_URL=https://9000-$WEB_HOST|" .env
+sed -i "s|^APP_URL=.*|APP_URL=https://9002-$WEB_HOST|" .env
+sed -i "s|^ASSET_URL=.*|ASSET_URL=https://9002-$WEB_HOST|" .env
 sed -i "s|^HMR_HOST=.*|HMR_HOST=5173-$WEB_HOST|" .env
 echo "alias test='bun run build && mv public/hot public/hot-bak && xvfb-run composer test && mv public/hot-bak public/hot && rm -r public/build'" > ~/.bash_aliases
