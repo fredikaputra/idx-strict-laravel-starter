734509824bae79a2a662ce15d192c1817caeca2c
Fredika Putra <53744519+fredikaputra@users.noreply.github.com>
2026-03-14 11:23:23 +0700

    Update

--- bootstrap.sh
+++ bootstrap.sh
@@ -33,36 +33,37 @@ case "$kit" in
     git clone --depth 1 https://github.com/juststeveking/kit "$out" && rm -rf "$out/.git"
     ;;
 "wave")
-    curl -LO https://devdojo.com/wave/download && unzip download && mv thedevdojo-wave-* "$out"
+    curl -LO https://devdojo.com/wave/download && unzip download -d "$out"
+    # curl -LO https://devdojo.com/wave/download && unzip download && mv thedevdojo-wave-* "$out"
     ;;
 *)
     composer create-project laravel/laravel "$out" --remove-vcs --prefer-dist --no-scripts
     ;;
 esac
 
-mkdir -p "$out/.idx"
-cp setup.sh "$out/.idx/"
-j2 ./devNix.j2 -o "$out/.idx/dev.nix"
+# mkdir -p "$out/.idx"
+# cp setup.sh "$out/.idx/"
+# j2 ./devNix.j2 -o "$out/.idx/dev.nix"
 
-[[ $kit != "api" ]] && bun ./patch-vite.ts "$out"
+# [[ $kit != "api" ]] && bun ./patch-vite.ts "$out"
 
-cd "$out"
+# cd "$out"
 
-if [[ "$kit" != "api" ]]; then
-    echo "" >>.env.example
-    echo 'ASSET_URL="''${APP_URL}"' >>.env.example
-    echo "" >>.env.example
-    echo "HMR_HOST=" >>.env.example
-    echo "HMR_PORT=443" >>.env.example
-    echo "HMR_PROTOCOL=wss" >>.env.example
-fi
+# if [[ "$kit" != "api" ]]; then
+#     echo "" >>.env.example
+#     echo 'ASSET_URL="''${APP_URL}"' >>.env.example
+#     echo "" >>.env.example
+#     echo "HMR_HOST=" >>.env.example
+#     echo "HMR_PORT=443" >>.env.example
+#     echo "HMR_PROTOCOL=wss" >>.env.example
+# fi
 
-cp .env.example .env
+# cp .env.example .env
 
-if [[ "$kit" != "wave" ]]; then
-    sed -i 's/\("php": "[^0-9]*\)8\.[0-9]\+/\18.4/' composer.json
-    composer setup
-    composer update
-fi
+# if [[ "$kit" != "wave" ]]; then
+#     sed -i 's/\("php": "[^0-9]*\)8\.[0-9]\+/\18.4/' composer.json
+#     composer setup
+#     composer update
+# fi
 
-[[ $kit == "filament" ]] && composer require filament/filament && php artisan filament:install --panels
+# [[ $kit == "filament" ]] && composer require filament/filament && php artisan filament:install --panels
